90a47e1d164ba9ed015feb68ed1a0e2b21739154
jimmy.zhuang <zhuangjiaqi@rongyi.com>
2018-03-19 20:35:32 +0800

    no message

--- routes/api/zsh.js
+++ routes/api/zsh.js
@@ -27,17 +27,17 @@ router.use(async (ctx, next) => {
 
 // 1. 获取终端数
 router.post('/getTerminalCounting', result({
-	'terminalCounting|2-50': 4
+	'terminalCounting|2-50': 0,
 }))
 
 // 2. 获取电子货架概要数据
 router.post('/getESSummaryData', result({
-	'totalPopulation|10000-300000': 20000,
-	'clickPopulation|10000-300000': 20000,
-	'scanPopulation|2000-50000':    10000,
-	'orderNum|200-5000':            1000,
-	'orderAmount|200000-5000000':   10000,
-	'averageStayTime|2-50':         4,
+	'totalPopulation|10000-300000': 0,
+	'clickPopulation|10000-300000': 0,
+	'scanPopulation|2000-50000':    0,
+	'orderNum|200-5000':            0,
+	'orderAmount|200000-5000000':   0,
+	'averageStayTime|2-50':         0,
 }))
 
 // 3. 获取电子货架周点击数据
@@ -47,7 +47,7 @@ router.post('/getESWeeklyClickData', result({
 			'firstDayOfWeek|+1': ['2017-11-29', '2018-01-29', '2018-01-21'],
 			'lastDayOfWeek|+1':  ['2018-03-04', '2018-02-04', '2018-03-10']
 		},
-		'clickPopulation|500-2000': 100
+		'clickPopulation|500-2000': 0
 	}]
 }))
 
@@ -58,9 +58,9 @@ router.post('/getESWeeklyData', result({
 			'firstDayOfWeek|+1': ['2017-11-29', '2018-01-29', '2018-01-21'],
 			'lastDayOfWeek|+1':  ['2018-03-04', '2018-02-04', '2018-03-10']
 		},
-		'clickPopulation|500-2000': 100,
-		'scanPopulation|50-500': 100,
-		'orderNum|20-100': 10
+		'clickPopulation|500-2000': 0,
+		'scanPopulation|50-500': 0,
+		'orderNum|20-100': 0
 	}]
 }))
 
@@ -69,8 +69,8 @@ router.post('/getESUserGenderPopulation', result({
 	'dataList|2': [{
 		'gender|+1': [1, 2],
 		'genderDesc|+1': ['男', '女'],
-		'population|3000-100000': 10000,
-		'ratio|20-80': 20
+		'population|3000-100000': 0,
+		'ratio|20-80': 0
 	}]
 }))
 
@@ -79,20 +79,20 @@ router.post('/getESUserAgePopulation', result({
 	'dataList|9': [{
 		'age|+1': 0,
 		'ageDesc|+1': ['未知', '0-17岁', '18-24岁', '25-29岁', '30-34岁', '35-39岁', '40-49岁', '50-59岁', '60岁及以上',],
-		'population|2000-15000': 1000,
-		'ratio|5-50': 20
+		'population|2000-15000': 0,
+		'ratio|5-50': 0
 	}],
-	'totalPopulation|50000-100000': 2000
+	'totalPopulation|50000-100000': 0
 }))
 
 // 7. 获取电子货架人群画像（场景标签）汇总数据
 router.post('/getESUserSceneTagPopulation', result({
 	'dataList|5': [{
 		'sceneTag|+1': ['养身', '女装', '户外运动', '母婴', '汽车', ],
-		'population|2000-15000': 1000,
-		'ratio|5-50': 20
+		'population|2000-15000': 0,
+		'ratio|5-50': 0
 	}],
-	'totalPopulation|50000-100000': 2000
+	'totalPopulation|50000-100000': 0
 }))
 
 // 8. 获取电子货架运营汇总数据
@@ -102,10 +102,10 @@ router.post('/getESOperationSummaryData', result({
 		'endDate': '2018-02-28',
 		'mall': '中石化',
 		'terminalNum': 1,
-		'totalPopulation|20000-500000': 251282,
-		'stayPopulation': 71210,
-		'averageStayTime': 4,
-		'clickPopulation': 29061,
+		'totalPopulation|20000-500000': 0,
+		'stayPopulation': 0,
+		'averageStayTime': 0,
+		'clickPopulation': 0,
 		'scanPopulation': 11607,
 		'orderNum': 1136,
 		'orderRatio': 9.79,
